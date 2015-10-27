a3799789c88ace863b35b7d79acd0486e4f66ad5
Moshe Katz <moshe@ymkatz.net>
2015-10-27 15:04:36 -0400

    cleanup

--- system.config.js
+++ system.config.js
@@ -1,38 +1,38 @@
 System.config({
-  baseURL: "/",
+  baseURL: '/',
   defaultJSExtensions: true,
-  transpiler: "none",
+  transpiler: 'babel',
   paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
+    'github:*': 'jspm_packages/github/*',
+    'npm:*': 'jspm_packages/npm/*'
   },
 
   map: {
-    "knockout": "npm:knockout@3.3.0",
-    "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
+    'knockout': 'npm:knockout@3.3.0',
+    'github:jspm/nodelibs-assert@0.1.0': {
+      'assert': 'npm:assert@1.3.0'
     },
-    "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
+    'github:jspm/nodelibs-process@0.1.2': {
+      'process': 'npm:process@0.11.2'
     },
-    "github:jspm/nodelibs-util@0.1.0": {
-      "util": "npm:util@0.10.3"
+    'github:jspm/nodelibs-util@0.1.0': {
+      'util': 'npm:util@0.10.3'
     },
-    "npm:assert@1.3.0": {
-      "util": "npm:util@0.10.3"
+    'npm:assert@1.3.0': {
+      'util': 'npm:util@0.10.3'
     },
-    "npm:inherits@2.0.1": {
-      "util": "github:jspm/nodelibs-util@0.1.0"
+    'npm:inherits@2.0.1': {
+      'util': 'github:jspm/nodelibs-util@0.1.0'
     },
-    "npm:knockout@3.3.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    'npm:knockout@3.3.0': {
+      'process': 'github:jspm/nodelibs-process@0.1.2'
     },
-    "npm:process@0.11.2": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    'npm:process@0.11.2': {
+      'assert': 'github:jspm/nodelibs-assert@0.1.0'
     },
-    "npm:util@0.10.3": {
-      "inherits": "npm:inherits@2.0.1",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+    'npm:util@0.10.3': {
+      'inherits': 'npm:inherits@2.0.1',
+      'process': 'github:jspm/nodelibs-process@0.1.2'
     }
   }
 });
